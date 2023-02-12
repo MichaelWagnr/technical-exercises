dda186ed358676ee8e4d379a5accf692ad7feef8
Michael Wagner <michaelwagner780@gmail.com>
2023-02-12 12:52:22 -0500

    sameFreqOfDigits

--- TypeScript/udemy/sameFreqOfDigits.ts
+++ TypeScript/udemy/sameFreqOfDigits.ts
@@ -1,4 +1,4 @@
-//: Frequency Counter
+// Frequency Counter
 // Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
 
 // Your solution MUST have the following complexities:
