d20aafec5ab589f1e436022dab5a7c042ee24ff0
Michael Wagner <michaelwagner780@gmail.com>
2023-02-16 11:11:55 -0500

    counting duplicates

---
+++ TypeScript/codewars/6kyu/countingDuplicates.ts
@@ -0,0 +1,41 @@
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+// pass //
+
+function duplicateCount(text: string): number {
+	if (!text[0]) return 0
+
+	const freqCount: { [key: string]: number } = {}
+	const textArr = [...text.toLowerCase()]
+
+	textArr.forEach((char) => {
+		freqCount[char] = freqCount[char] + 1 || 1
+	})
+
+	let result = 0
+
+	for (const char in freqCount) {
+		if (freqCount[char] > 1) result += 1
+	}
+
+	return result
+}
+
+// solution //
+
+// function duplicateCount(text: string): number{
+//   const values = text.toLowerCase();
+//   const distinctValues = [... new Set(values)];
+//   const count = (s: string) => values.split(s).length - 1 > 1 ;
+//   return distinctValues.filter(value => count(value)).length;
+// }
