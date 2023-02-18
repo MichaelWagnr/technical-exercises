2e0a0557529b446918e6415dd80ef62f2fdd85e8
Michael Wagner <michaelwagner780@gmail.com>
2023-02-17 19:12:18 -0500

    hex to rgb

---
+++ TypeScript/codewars/5kyu/hexToRgb.ts
@@ -0,0 +1,31 @@
+// When working with color values it can sometimes be useful to extract the individual red, green, and blue (RGB) component values for a color. Implement a function that meets these requirements:
+
+// Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
+// Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+// Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
+
+// Example
+// "#FF9933" --> {r: 255, g: 153, b: 51}
+
+// pass //
+
+function hexStringToRGB(hexString: string) {
+	const result = { r: 0, g: 0, b: 0 }
+	const arr = hexString.match(/\w{2}/gi)!
+
+	result.r = parseInt(arr[0], 16)
+	result.g = parseInt(arr[1], 16)
+	result.b = parseInt(arr[2], 16)
+
+	return result
+}
+
+// solution //
+
+// function hexStringToRGB(h) {
+//   return {
+//     r: parseInt(h.slice(1,3), 16),
+//     g: parseInt(h.slice(3,5), 16),
+//     b: parseInt(h.slice(5,7), 16)
+//   };
+// }
